7b7de94cd16c7cedf2032ea0985bc2ed4b3a9105
Robert Gibb <rgibb@rethinkrobotics.com>
2018-08-18 08:07:59 -0400

    Added ReadMe and some opcode instruction functions

---
+++ Readme.md
@@ -0,0 +1,5 @@
+JavaScript Chip-8 Emulator
+
+Reading Material:
+Chip-8 Technical Reference http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1
+Chip-8 JavaScript Tutorial http://blog.alexanderdickson.com/javascript-chip-8-emulator
--- chip8.js
+++ chip8.js
@@ -39,7 +39,7 @@ class chip8 {
             const { memory, pc } = this.state;
             const opcode = memory[pc] << 8 | memory[pc + 1];
             const instruction = getInstruction(opcode);
-            instruction(this.state);
+            instruction(opcode, this.state);
         }
 
         window.requestAnimationFrame(this.run);
@@ -52,3 +52,47 @@ function getInstruction(opcode) {
         
     }
 }
+
+const instructionMap = {
+    // 0x0nnn - SYS addr
+    0x0: (opcode, { pc }) => {
+        pc = opcode;
+    },
+    // 0x1nnn - JP addr
+    0x1: (opcode, { pc }) => {
+        pc = opcode & 0x0FFF;
+    },
+    // 0x2nnn - CALL addr 
+    0x2: (opcode, { pc, stack, sp }) => {
+        stack[sp] = pc;
+        sp += 1;
+        pc = opcode & 0x0FFF;
+    },
+    // 0x3xkk - SE Vx, byte
+    0x3: (opcode, { v, pc }) => {
+        const register = opcode & 0x0F00;
+        const compareVal = opcode & 0x00FF; 
+        
+        if (v[register] === compareVal) {
+            pc += 2;
+        }
+    },
+    // 0x4xkk SNE Vx, byte
+    0x4: (opcode, { v, pc }) => {
+        const register = opcode & 0x0F00;
+        const compareVal = opcode & 0x00FF; 
+        
+        if (v[register] !== compareVal) {
+            pc += 2;
+        }
+    },
+    // 0x5xy0 SNE Vx, byte
+    0x5: (opcode, { v, pc }) => {
+        const x = opcode & 0x0F00;
+        const y = opcode & 0x00F0; 
+        
+        if (x === y) {
+            pc += 2;
+        }
+    }
+};
