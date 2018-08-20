511f5ea8ebaf959f1ce1747c4f7549d3c31bdcd9
Robert Gibb <rgibb@rethinkrobotics.com>
2018-08-19 22:08:59 -0400

    Adding more instructions

--- chip8.js
+++ chip8.js
@@ -28,10 +28,12 @@ class chip8 {
 
     constructor() {
         this.state = JSON.parse(JSON.stringify(state));
+        this.state.stack[0] = 0;
     }
 
     reset() {
         this.state = JSON.parse(JSON.stringify(state));
+        this.state.stack[0] = 0;
     }
 
     run() {
@@ -58,14 +60,23 @@ const instructionMap = {
     0x0: (opcode, { pc }) => {
         pc = opcode;
     },
+    // 00E0 - CLS
+    0x00E0: () => {
+        // clear the display
+    },
+    // 0x00EE - RET
+    0x00EE: (opcode, { pc, stack, sp }) => {
+        pc = stack[sp];
+        sp -= 1;
+    },
     // 0x1nnn - JP addr
     0x1: (opcode, { pc }) => {
         pc = opcode & 0x0FFF;
     },
     // 0x2nnn - CALL addr 
     0x2: (opcode, { pc, stack, sp }) => {
-        stack[sp] = pc;
         sp += 1;
+        stack[sp] = pc;
         pc = opcode & 0x0FFF;
     },
     // 0x3xkk - SE Vx, byte
@@ -94,5 +105,55 @@ const instructionMap = {
         if (x === y) {
             pc += 2;
         }
-    }
+    },
+    // 0x6xkk - LD Vx, byte
+    0x6: (opcode, { v }) => {
+        const data = opcode & 0x00FF;
+        const register = opcode & 0x0F00;
+
+        v[register] = data;
+    },
+    // 0x7xkk - ADD Vx, byte
+    0x7: (opcode, { v }) => {
+        const data = opcode & 0x00FF;
+        const register = opcode & 0x0F00;
+
+        v[register] += data;
+    },
+    // 8xy0 - LD Vx, Vy
+    0x8000: (opcode, { v }) => {
+        const x = opcode & 0x0F00;
+        const y = opcode & 0x00F0;
+        
+        v[x] = v[y];
+    },
+    // 8xy1 - OR Vx, Vy
+    0x8001: (opcode, { v }) => {
+        const x = opcode & 0x0F00;
+        const y = opcode & 0x00F0;
+        
+        v[x] = v[x] | v[y];
+    },
+    // 8xy2 - AND Vx, Vy
+    0x8002: (opcode, { v }) => {
+        const x = opcode & 0x0F00;
+        const y = opcode & 0x00F0;
+        
+        v[x] = v[x] & v[y];
+    },
+    // 8xy3 - XOR Vx, Vy
+    0x8003: (opcode, { v }) => {
+        const x = opcode & 0x0F00;
+        const y = opcode & 0x00F0;
+        
+        v[x] = v[x] | v[y];
+    },
+    // 8xy4 - ADD Vx, Vy
+    0x8004: (opcode, { v }) => {
+        const x = opcode & 0x0F00;
+        const y = opcode & 0x00F0;
+        
+        v[x] = v[x] | v[y];
+    },
+
 };
